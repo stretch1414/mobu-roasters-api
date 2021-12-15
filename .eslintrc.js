cac86706eb519d220fd2dfc845a71d76641a3ee5
stretch1414 <jmoser1414@gmail.com>
2021-12-14 22:59:06 -0500

    Add graphql-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,4 +16,14 @@ export const config = {
   rules: {
     'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
   },
+  overrides: [
+    {
+      files: ['*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      plugins: ['@graphql-eslint'],
+      rules: {
+        '@graphql-eslint/known-type-names': 'error',
+      },
+    },
+  ],
 };
