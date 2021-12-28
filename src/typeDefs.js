4f79a3d0b39bca49567b95e7ab559ac391ff385d
stretch1414 <jmoser1414@gmail.com>
2021-12-28 17:52:23 -0500

    Remove unused dependency

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,6 +1,5 @@
 import { loadFiles } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
-import { print } from 'graphql';
 
 const typesArray = await loadFiles('**/*.graphql');
 
