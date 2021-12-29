bd4d8f47ef849e7e641569daad7d160f68f07a1b
stretch1414 <jmoser1414@gmail.com>
2021-12-29 10:12:10 -0500

    Cleanup old code and console logs

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,31 +1,10 @@
 import glob from 'glob';
-import { loadFiles } from '@graphql-tools/load-files';
 import { mergeResolvers } from '@graphql-tools/merge';
-// import { resolvers } from './Book';
 
 import path from 'path';
 import { fileURLToPath, pathToFileURL } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// const DEFAULT_SCOPES = ['USER'];
-
-// const scopesMap = {};
-
-// console.log('resolvers', resolvers);
-
-// const resolversArray = await loadFiles('**/Book.js', {
-//   // onlyFiles: false,
-//   // globOptions: { absolute: false },
-//   ignore: ['./index.js'],
-// });
-
-// console.log('resolversArray', { resolversArray });
-
-// const mergedResolvers = mergeResolvers(resolversArray);
-
-// console.log('mergedResolvers', { mergedResolvers });
-// console.log(JSON.stringify(mergedResolvers));
 
 // Have to use globbing instead because of https://github.com/nodejs/node/issues/31710
 const resolversArray = await Promise.all(
@@ -34,16 +13,10 @@ const resolversArray = await Promise.all(
     .map((file) => import(pathToFileURL(path.resolve(__dirname, file))))
 );
 
-console.log('resolversArray', resolversArray);
-
 const mergedResolvers = mergeResolvers(
   resolversArray.map((resolverMap) => {
-    console.log('resolverMap', resolverMap);
     return resolverMap.default;
   })
 );
-
-console.log('mergedResolvers', mergedResolvers);
-console.log(JSON.stringify(mergedResolvers));
 
 export default mergedResolvers;
